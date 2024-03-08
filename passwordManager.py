fa8d3fa28ea1cb3f9489ccc24d7a49a8b0e70299
Farrellio Fawzi (Farrell) <160052961+farrell-fawzi@users.noreply.github.com>
2024-03-08 12:20:39 -0500

    Add files via upload

---
+++ passwordManager.py
@@ -0,0 +1,17 @@
+from cryptography.fernet import Fernet
+
+
+class PasswordManager:
+    
+    def __init__(self):
+        self.key = None
+        self.password_file = None
+        self.password_dick = {}
+
+    def create_key(self, path):
+        self.key = Fernet.generate_key()
+        with open(path, 'wb') as f:
+            f.write(self.key)
+
+pm = PasswordManager()
+pm.create_key("mykey.key")            
